1faef00deb368605dc08531d8b8e7b86cc233720
Ronnie <148753940+Nessumsa@users.noreply.github.com>
2024-03-14 13:29:55 +0100

    Changed numberaxis to categoryaxis plus months etc

--- src/main/java/com/example/uge11/HelloApplication.java
+++ src/main/java/com/example/uge11/HelloApplication.java
@@ -26,37 +26,16 @@ public class HelloApplication extends Application {
     @Override public void start(Stage stage) throws FileNotFoundException {
         stage.setTitle("Line Chart Sample");
         //defining the axes
-        final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
-        final CategoryAxis xxAxis = new CategoryAxis();
-        xAxis.setLabel("Number of Month");
+        final CategoryAxis xAxis = new CategoryAxis();
         //creating the chart
         final LineChart<String,Number> lineChart =
-                new LineChart<String,Number>(xxAxis,yAxis);
+                new LineChart<String,Number>(xAxis,yAxis);
 
         lineChart.setTitle("Stock Monitoring, 2010");
-        xAxis.setAutoRanging(false);
-        xAxis.setLowerBound(0.5);
-        xAxis.setUpperBound(12);
-        xAxis.setTickUnit(1);
         yAxis.setAutoRanging(false);
-        yAxis.setTickUnit(500);
-        xxAxis.setEndMargin(0.5);
-
-
-
-
-        //defining a series
-        //XYChart.Series series = new XYChart.Series();
-        //XYChart.Series series2 = new XYChart.Series();
-        //XYChart.Series series3 = new XYChart.Series();
-
-        //series.setName("Bucket 1");
-        //series2.setName("Bucket 2");
-        //series3.setName("Bucket 3");
-        //populating the series with data
-
-        //series.getData().add(new XYChart.Data(1, 23));
+        yAxis.setTickUnit(250);
+        xAxis.setEndMargin(0.5);
 
         File directory = new File("C:/test/memeTest");
 
@@ -66,51 +45,33 @@ public class HelloApplication extends Application {
             listOfSeries.add(new XYChart.Series());
         }
 
-
-        /*scanner = new Scanner(directory);
-        if(scanner.hasNextLine())
-        {
-            scanner.nextLine();
-        }
-        while(scanner.hasNextLine())
-        {
-
-            String line = scanner.nextLine();
-            String[] tempArray = line.split("[:,]");
-            for (int i = 0; i < tempArray[0].length(); i++) {
-                System.out.println(tempArray[8]);
-            }
-            data.add(new Record(tempArray[0],
-                            tempArray[1],
-                            Integer.parseInt(tempArray[2]),
-                            tempArray[3],
-                            Integer.parseInt(tempArray[4]),
-                            Integer.parseInt(tempArray[5]),
-                            Integer.parseInt(tempArray[6]),
-                            Integer.parseInt(tempArray[7]),
-                            Integer.parseInt(tempArray[8]))
-            );
-        }*/
         for(int j = 0; j < buckets.size(); j++) {
             int tempLowerBound = 90001;
             int tempUpperBound = 0;
             for (int i = 1; i <= 12; i++) {
                 int tempTotal = 0;
+                int totelWeight = 0;
                 for (Record s : buckets.get(j).getData()) {
                     if (s.month == i) {
-                        tempTotal += Integer.parseInt((s.weight.split(" "))[0]);
+                        if(tempTotal >= Integer.parseInt(s.weight)){ //Remove '=' if trash is not emptied if the same value
+                            totelWeight += tempTotal;
+                            tempTotal = Integer.parseInt(s.weight);
+                        }
+                        else{
+                            tempTotal = Integer.parseInt(s.weight);
+                        }
+                        //tempTotal += Integer.parseInt(s.weight);
                     }
                 }
-                System.out.println("Month: " + i + " total weight: " + tempTotal);
-                //series.getData().add(new XYChart.Data(1, 23)
-                listOfSeries.get(j).getData().add(new XYChart.Data(new DateFormatSymbols().getMonths()[i-1], tempTotal));
+                System.out.println("Month: " + i + " total weight: " + totelWeight);
+                listOfSeries.get(j).getData().add(new XYChart.Data(new DateFormatSymbols().getMonths()[i-1], totelWeight));
                 if(tempTotal < tempLowerBound){
                     tempLowerBound = tempTotal;
-                    yAxis.setLowerBound(tempTotal-500);
+                    yAxis.setLowerBound(totelWeight-500);
                 }
                 if(tempTotal > tempUpperBound){
                     tempUpperBound = tempTotal;
-                    yAxis.setUpperBound(tempTotal+500);
+                    yAxis.setUpperBound(totelWeight+500);
                 }
             }
             listOfSeries.get(j).setName((buckets.get(j).getFileName().split("[.]"))[0]);
@@ -122,8 +83,6 @@ public class HelloApplication extends Application {
             lineChart.getData().add(s);
 
         }
-
-
 
         stage.setScene(scene);
         stage.show();
